b35acceffb7d0efa21a618d0ff25f2639df52327
federus1105 <federusrudi@gmail.com>
2025-11-26 14:42:22 +0700

    Feat ( routes ) : feature init validation for product routes

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import productControllers from '../controllers/products.controllers.js'
 import upload from '../libs/upload.js'
+import { addProduct } from '../controllers/products.controllers.js';
+import { productValidation } from '../utils/validators.js';
+import { validate } from '../libs/validate.js';
 
 const router = express.Router();
 
+router.post('/', productValidation,validate, addProduct)
 router.get('/', productControllers.getListProduct);
 router.get('/:id', productControllers.getProductById);
 router.delete('/:id', productControllers.deleteproducbyId);
-router.post('/', productControllers.addProduct)
 router.patch('/:id', productControllers.editProduct)
 router.post('/:id/upload', upload.single('image'), productControllers.uploadProductImage);
 
 
-export default router 
+export default router;
