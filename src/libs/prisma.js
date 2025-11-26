6a64cc9172b2aadf787d2fe8230a20b09e27621b
federus1105 <federusrudi@gmail.com>
2025-11-26 13:56:17 +0700

    Feat ( libs ) : feature init prisma

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,9 +1,13 @@
-let prisma;
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import "dotenv/config";
+import { env } from "node:process";
+import { PrismaClient } from "../../generated/prisma/index.js";
 
-export async function getPrisma() {
-  if (!prisma) {
-    const { PrismaClient } = await import('../generated/prisma/index.js');
-    prisma = new PrismaClient();
-  }
-  return prisma;
-}
+const connectionString = `${env.DATABASE_URL}`;
+
+const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
+
+export const getPrisma = () => prisma;
