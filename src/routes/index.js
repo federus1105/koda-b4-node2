d75961a88afe64959a79bf107039b8217ed36f78
federus1105 <federusrudi@gmail.com>
2025-11-26 17:12:01 +0700

    Feat ( routes ) : feature jwt token in route product

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./auth.router.js"
 import productRoutes from "./products.router.js"
+import authMiddleware from "../middleware/auth.midlleware.js";
 
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.use('/product', productRoutes);
+router.use('/product', authMiddleware, productRoutes);
 
 export default router;
